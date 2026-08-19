9d42524d26e33329edbd20f195f602826439ee30
Gustavo Gines <ginesparker95@gmail.com>
2026-08-19 01:44:08 -0300

    fix: ancho del carrito en movil

--- resources/views/livewire/cart-panel.blade.php
+++ resources/views/livewire/cart-panel.blade.php
@@ -189,8 +189,7 @@ new class extends Component {
                      x-transition:enter-end="translate-x-0" 
                      x-transition:leave="transform transition ease-in-out duration-500 sm:duration-700" 
                      x-transition:leave-start="translate-x-0" 
-                     x-transition:leave-end="translate-x-full" 
-                     class="pointer-events-auto w-screen max-w-md">
+                     class="pointer-events-auto w-full max-w-md">
                      
                      <div class="flex h-full flex-col relative shadow-2xl transition-colors duration-300 bg-white dark:bg-gray-900 border-l border-gray-200 dark:border-gray-800" >
                         <div class="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
